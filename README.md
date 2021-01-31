be76915b70f3db5ceb0c083b986add0a7ac604fd
Joshua Newhouse <joshua.b.newhouse@gmail.com>
2021-01-31 12:24:05 -0600

    Implemented AStarNode

--- README.md
+++ README.md
@@ -3,9 +3,8 @@
 For testing run with -t followed by some valid initial state; ex: -t WBxWB
 
 ## TODO
-1. Implement AStarNode
-2. Implement movement cost and fix command line args
-3. Output all nodes put to queue
-4. Try to break and fix any errors
-5. Investigate possible bug in DFS
+1. Implement movement cost and fix command line args
+2. Output all nodes put to queue
+3. Try to break and fix any errors
+4. Investigate possible bug in DFS
    ```WWWWWxBBBBB finds solution w/MAX_DEPTH == 21 but not MAX_DEPTH == 22+```
--- src/main/java/edu/utdallas/cs6301/tileordering/node/AStarNode.java
+++ src/main/java/edu/utdallas/cs6301/tileordering/node/AStarNode.java
@@ -3,12 +3,18 @@ package edu.utdallas.cs6301.tileordering.node;
 import edu.utdallas.cs6301.tileordering.StateEnumerable;
 
 public class AStarNode<T extends StateEnumerable<T>> extends Node<T> {
+    private final int gValue;
+    private final int hValue;
+
     public AStarNode(Node<T> parent, int depth, T item, int ID) {
         super(parent, depth, item, ID);
+
+        this.gValue = parent == null ? 0 : parent.getCost() + item.getCost(parent.getItem());
+        this.hValue = item.getDistanceFromGoal();
     }
 
     @Override
     protected int getCost() {
-        return 0;
+        return this.gValue + this.hValue;
     }
 }
